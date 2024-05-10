74a389a48bdc2abdcb8f285e34f2ac41c1ec217b
Yusuf Tan <yusuftan41@hotmail.com>
2024-05-10 17:33:08 +0300

    Dosya yapısı oluşturuldu, Navbar geliştirmesine başlandı.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
+      <h1 className="w-full text-3xl font-bold text-[#57dfe9]">LOGO</h1>
+      <ul className="flex hidden">
+        <li className="p-4">Home</li>
+        <li className="p-4">Movies</li>
+        <li className="p-4">Contact</li>
+      </ul>
+      <div>
+        <AiOutlineMenu size={20}/>
+      </div>
+      <div className='fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300]'>
+      <h1 className="w-full text-3xl font-bold text-[#57dfe9] m-4">LOGO</h1>
+        <ul className='p-4 uppercase'>
+            <li className="p-4 border-b border-gray-600">Home</li>
+            <li className="p-4 border-b border-gray-600">Movies</li>
+            <li className="p-4 border-b border-gray-600">Contact</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
